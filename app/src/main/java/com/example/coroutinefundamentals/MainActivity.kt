2554fddf69952c38167048ea7eeb6f5fba0bb540
shadabhashmi21 <er.shadabhashmi@gmail.com>
2023-06-12 12:01:02 +0530

    feat: async keyword

--- app/src/main/java/com/example/coroutinefundamentals/MainActivity.kt
+++ app/src/main/java/com/example/coroutinefundamentals/MainActivity.kt
@@ -8,6 +8,7 @@ import android.widget.TextView
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.yield
 
@@ -21,49 +22,35 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         counterText = findViewById(R.id.counter_text)
 
-        CoroutineScope(Dispatchers.Main).launch {
-            task1()
-            task2()
-        }
         /*CoroutineScope(Dispatchers.Main).launch {
+            task1()
+        }
+        CoroutineScope(Dispatchers.Main).launch {
             task2()
         }*/
-    }
-
-    /*fun updateCounter(view: View) {
-        counterText.text = counter++.toString();
-    }*/
-    /*private fun executeLongRunningTask() {
-        for (i in 1..10000000000L){
-
-        }
-        Log.d(tag, "Task Completed")
-    }*/
-
-    /*fun executeTaskEfficiently(view: View){
-        CoroutineScope(Dispatchers.IO).launch {
-            Log.d(tag, Thread.currentThread().name)
-            executeLongRunningTask()
-        }
 
         CoroutineScope(Dispatchers.Main).launch {
-            Log.d(tag, Thread.currentThread().name)
+            val job = async {
+                getRandomNumber()
+            }
+            Log.d(tag, job.await().toString())
         }
+    }
 
-        CoroutineScope(Dispatchers.Default).launch {
-            Log.d(tag, Thread.currentThread().name)
-        }
-    }*/
-
-    private suspend fun task1(){
+    private suspend fun task1() {
         Log.d(tag, "Task 1 Started")
         yield()
         Log.d(tag, "Task 1 Completed")
     }
 
-    suspend fun task2(){
+    private suspend fun task2() {
         Log.d(tag, "Task 2 Started")
         yield()
         Log.d(tag, "Task 2 Completed")
+    }
+
+    private suspend fun getRandomNumber(): Int {
+        yield()
+        return (0..100).random()
     }
 }
